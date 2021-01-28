56ef9ad6327b53bd713e672d5f6d0c4a4abfea2d
Alexis Marroquin <alexismarroquin754@gmail.com>
2021-01-28 12:10:33 -0800

    starting task 1

--- index.js
+++ index.js
@@ -41,9 +41,17 @@ class Airplane {
           + It should return a string with `name` and `age`. Example: "Mary, 50"
   */
   
- class Person {
+class Person {
+  constructor(name, age){
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
+  eat(){
     
   }
+    
+}
   
   /*
     TASK 2
